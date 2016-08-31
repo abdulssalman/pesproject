ea9fa2121b5a1056879d83aacdc8876ae911b2d3
Krishna Madgula <krishnac.madgula@gmail.com>
2016-08-31 20:56:58 +0530

    some commit

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ class Config:
     WTF_CSRF_ENABLED = True
     MONGO_DBNAME= 'UserData'
     DB_NAME = 'UserData'
-    DB=MongoClient()[DB_NAME]
+    DB=MongoClient('')[DB_NAME]
     USER_COLLECTION=DB.users
     DEBUG=True
